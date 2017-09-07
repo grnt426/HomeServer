a4c8c9c7ab712d0aefca5ac3b0177d8d9e0373a5
Grant Kurtz <kurtz.grant@gmail.com>
2017-09-06 23:48:21 -0400

    Keystore no longer required for development clients.
    * New environment variable "keystoreLocation" must be specified
    in order to serve HTTPS clients.

--- src/main/java/app/WebConfig.java
+++ src/main/java/app/WebConfig.java
@@ -20,6 +20,7 @@ class WebConfig {
 	private static final String AUTH_HEADER_NAME = System.getenv("authHeaderName");
 	private static final String AUTH_HEADER_VALUE = System.getenv("authHeaderValue");
 	private static final String KEYSTORE_PASSWORD = System.getenv("keystorePassword");
+	private static final String KEYSTORE_LOCATION = System.getenv("keystoreLocation");
 	private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
 	private final AcController acController;
@@ -42,7 +43,11 @@ class WebConfig {
 		map.put(RythmConfigurationKey.HOME_TEMPLATE.getKey(), System.getProperty("user.dir") + "/resources/templates");
 		Rythm.init(map);
 
-		secure("/home/granite/HomeServer/letsencrypt.p12", KEYSTORE_PASSWORD, null, null);
+		if (KEYSTORE_LOCATION == null) {
+			logger.info("Starting up in devo. Serving only HTTP clients.");
+		} else {
+			secure(KEYSTORE_LOCATION, KEYSTORE_PASSWORD, null, null);
+		}
 		port(8443);
 
 		before("/*", (req, res) -> {
