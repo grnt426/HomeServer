12114c8018f079c3a0556e2d4ff11506f005bba2
Grant Kurtz <kurtz.grant@gmail.com>
2017-10-18 18:30:26 -0400

    Changed from LocalDateTime to ZonedDateTime.

--- src/main/java/app/ambiance/AmbientMqttControllerFacade.java
+++ src/main/java/app/ambiance/AmbientMqttControllerFacade.java
@@ -4,7 +4,7 @@ import app.util.JsonTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -17,8 +17,8 @@ public class AmbientMqttControllerFacade {
 	public void syncDeviceState(String deviceId, String payload) {
 		AmbientState state = JsonTransformer.fromJson(payload, AmbientState.class);
 		state.setDeviceID(deviceId);
-		state.setEventTime(LocalDateTime.now().format(
-				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZ", Locale.ENGLISH)));
+		state.setEventTime(ZonedDateTime.now().format(
+				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZZ", Locale.ENGLISH)));
 		ambientDao.syncDeviceState(state);
 	}
 }
